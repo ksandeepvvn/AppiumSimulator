d93932b527bc0656752f5e62df22c700558a950f
Sandeep <sandeep.k@osmosys.asai>
2020-05-26 20:10:03 +0530

    Automate Signing Profile

--- AppiumTest/ViewController.swift
+++ AppiumTest/ViewController.swift
@@ -9,9 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {}
-
-//this is initiallising sdgsdjhsgdjagdas
-
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
